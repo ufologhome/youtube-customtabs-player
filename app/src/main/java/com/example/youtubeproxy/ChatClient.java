23c9431ca74dcb2147d3f484fe42463ac865d0c8
ufologhome <38780109+ufologhome@users.noreply.github.com>
2026-01-03 17:21:24 +0300

    Update ChatClient.java

--- app/src/main/java/com/example/youtubeproxy/ChatClient.java
+++ app/src/main/java/com/example/youtubeproxy/ChatClient.java
@@ -4,21 +4,24 @@ import java.io.*;
 import java.net.Socket;
 
 public class ChatClient {
+
     Socket socket;
     BufferedReader in;
     PrintWriter out;
 
-    public ChatClient(String host) throws Exception {
-        socket = new Socket(host, 9009);
+    public ChatClient(String host, int port) throws IOException {
+        socket = new Socket(host, port);
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out = new PrintWriter(socket.getOutputStream(), true);
+        out = new PrintWriter(new BufferedWriter(
+                new OutputStreamWriter(socket.getOutputStream())
+        ), true);
     }
 
     public void send(String msg) {
         out.println(msg);
     }
 
-    public String read() throws Exception {
+    public String read() throws IOException {
         return in.readLine();
     }
 }
